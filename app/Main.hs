a4c3ccff5159fa14664cd60f5dbae5315b0daaa0
Adrian Sieber <mail@adriansieber.com>
2018-07-15 09:44:03 +0000

    Add support for selecting corners

--- app/Main.hs
+++ app/Main.hs
@@ -5,33 +5,83 @@ module Main where
 
 import Graphics.Gloss
 import Graphics.Gloss.Juicy
+import Graphics.Gloss.Interface.Pure.Game as Gl
 import System.Environment
 import Protolude as P
 import Data.Text as T
 
 
-load :: FilePath -> IO ()
+type Corner = Point
+
+-- | State of app (list of corners is reversed to order of addition)
+data AppState = AppState [Corner] [Picture]
+  deriving Show
+
+
+load :: FilePath -> IO Picture
 load filePath = do
   picMaybe <- loadJuicy filePath
 
+  case picMaybe of
+    Nothing -> do
+      putText "Error: Image couldn't be loaded"
+      pure Blank
+    Just picture -> pure picture
+    -- display
+    --   (InWindow filePath (width, height) )
+    --   black
+    --   picture
+
+
+startApp :: FilePath -> Picture -> IO ()
+startApp filePath pic = do
   let
     width = 640
     height = 320
+    initialX = 800
+    initalY = 100
+    initalState = AppState [] [pic]
 
-  case picMaybe of
-    Nothing -> putText "Error: Image couldn't be loaded"
-    Just picture -> display
-      (InWindow filePath (width, height) (10,  10))
-      black
-      picture
+  putText "Starting the app …"
+
+  play
+    (InWindow filePath (width, height) (initialX, initalY))
+    black
+    10
+    initalState
+    makePicture
+    handleEvent
+    stepWorld
 
 
-frame :: Int -> Int -> Picture -> Float -> Picture
-frame width height picture t =
-  Color (greyN (abs $ sin (t * 2)))
-    $ Pictures [ rectangleSolid (fromIntegral width) (fromIntegral height)
-               , picture
-               ]
+stepWorld :: Float -> AppState -> AppState
+stepWorld _ = identity
+
+
+-- | Render the app state to a picture.
+makePicture :: AppState -> Picture
+makePicture (AppState corners pics) =
+  let
+    radius = 6
+    thickness = 4
+    drawCorner (x, y) =
+      Translate x y (color green $ ThickCircle radius thickness)
+  in
+    Pictures $ pics <> (fmap drawCorner corners)
+
+
+addCorner :: AppState -> Corner -> AppState
+addCorner (AppState corners pics) corner =
+  AppState (corner : corners) pics
+
+
+handleEvent :: Event -> AppState -> AppState
+handleEvent event appState =
+  case event of
+    EventKey (MouseButton LeftButton) Gl.Down _ point ->
+      addCorner appState point
+    _ ->
+      appState
 
 
 -- | Displays uncompressed 24/32 bit BMP images.
@@ -40,11 +90,9 @@ main = do
   args <- getArgs
 
   case args of
-    [filePath] ->
-           if "bmp" `T.isSuffixOf` (T.pack filePath) then load filePath
-      else if "jpg" `T.isSuffixOf` (T.pack filePath) then load filePath
-      else putText "This file extension is not supported"
-
+    [filePath] -> do
+      image <- load filePath
+      startApp filePath image
     _ ->
       putText $ T.unlines
         [ "usage: bitmap <file.bmp>"
