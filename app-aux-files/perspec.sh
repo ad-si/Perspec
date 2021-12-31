b1d6e764454cb8aa872c298acfbb3416ac762118
Adrian Sieber <mail@adriansieber.com>
2021-12-31 12:41:57 +0000

    Docs: Display better usage information when opening the macOS app

--- app-aux-files/perspec.sh
+++ app-aux-files/perspec.sh
@@ -1,1 +1,30 @@
-./perspec fix "$@"
+#! /usr/bin/env sh
+
+if test $# == 0
+then
+
+# Lines must contain at least one space or they are discarded
+printf " "
+printf "
+                                     USAGE
+────────────────────────────────────────────────────────────────────────────────
+ "
+printf "
+1. Drop the image file(s) onto this window
+2. Mark the corners by clicking on them
+3. Press [Enter]
+ "
+printf "
+You can also use it directly via the command line.
+ "
+printf "
+For more information open your terminal and run \`perspec help\`.
+(On macOS \`/Applications/Perspec.app/Contents/Resources/perspec help\`.)
+ "
+printf "
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+ "
+
+else
+  ./perspec fix "$@"
+fi
