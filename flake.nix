8b8ab68c74222b0b0df8ed63889c91b6ae303d05
Adrian Sieber <mail@adriansieber.com>
2025-06-14 18:27:54 +0000

    Use Nix Flake to manage dependencies

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      utils,
+    }:
+    utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            bash
+            coreutils
+            gnumake
+            imagemagick
+            libllvm
+            stack
+          ];
+        };
+        formatter = pkgs.nixfmt-tree; # Format this file with `nix fmt`
+      }
+    );
+}
